b7ae0274a6c728ff352cd9f7e8dbf811a37cedd5
mei-517 <meisuma@icloud.com>
2026-08-03 12:02:09 +0900

    Add p127~p140

---
+++ 3-1.R
@@ -0,0 +1,63 @@
+##20260803 p127~140
+
+##シンボルを抽出
+get_symbols <- function(){
+  wheel <- c("DD","7","BBB","BB","B","C","0")
+  sample(wheel, size = 3, replace = TRUE, prob = c(0.03,0.03,0.06,0.1,0.25,0.01,0.52))
+}
+
+get_symbols()
+
+##play関数
+play <- function(){
+  symbols <- get_symbols()
+  print(symbols)
+  score(symbols)
+}
+
+###選択
+
+##第１の条件
+same <- length(unique(symbols)) == 1
+
+#sameほかの例
+#symbols[1] ==symbols[2] & symbols[2] == symbols[3]
+#symbols == symbols[1]
+#length(unique(symbols)) == 1
+
+#テスト
+symbols <- c("7","7","7")
+same <- length(unique(symbols)) == 1
+same
+symbols <- c("7","B","7")
+same <- length(unique(symbols)) == 1
+same
+##第２の条件
+
+
+
+
+
+##if(same){
+##prize <- 賞金をルックアップ
+##}else if(すべてバー){
+##prize <- 5ドル割り当て
+##}else{
+##チェリーの数を計算
+##prize <- 賞金を計算
+##}
+
+##ダイヤを数える
+##必要に応じて賞金を２倍にする
+
+
+
+
+
+
+
+##計算
+score <- function(symbols){
+  #賞金計算
+    prize
+}
